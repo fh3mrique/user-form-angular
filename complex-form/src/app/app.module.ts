d72246116eb19b901ef32829a4ef265784ec1e33
Filipe Lima <fhemrique55@gmail.com>
2024-08-26 18:31:23 -0300

    Criação dos Módulos

--- complex-form/src/app/app.module.ts
+++ complex-form/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
