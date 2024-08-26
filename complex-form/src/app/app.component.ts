4c9b802f1ff972a920e378e87679a436ac818879
Filipe Lima <fhemrique55@gmail.com>
2024-08-26 20:58:20 -0300

    Criação do Service de Estados

--- complex-form/src/app/app.component.ts
+++ complex-form/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './services/countries.service';
+import { StatesService } from './services/states.service';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,19 @@ import { CountriesService } from './services/countries.service';
 })
 export class AppComponent implements OnInit {
   
-  constructor(private readonly _countriesService: CountriesService){}
+  constructor(
+    private readonly _countriesService: CountriesService,
+    private readonly _statesService: StatesService
+  ){}
   
   ngOnInit(){
     this._countriesService.getContries().subscribe((contriesResponse) => {
       console.log('contriesResponse', contriesResponse )
     });
+
+    this._statesService.getStates('Brazil').subscribe((stateResponse)=>{
+        console.log('stateResponse', stateResponse)
+    })
+
   }
 }
---
+++ complex-form/src/app/services/states.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+
+@Injectable({
+    providedIn: "root"
+})
+export class StatesService {
+    constructor(private readonly _httpClient: HttpClient){}
+
+    getStates (countryName: string){
+        return this._httpClient.post<any>(
+            'https://countriesnow.space/api/v0.1/countries/states',
+            {country: countryName}
+        ).pipe(
+            map((stateResponse)=>{
+                return stateResponse.data.states;
+            })
+        )
+    }
+}
