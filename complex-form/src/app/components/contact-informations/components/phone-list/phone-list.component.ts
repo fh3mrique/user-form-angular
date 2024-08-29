bbaedcdd21f5751fcd289cb4922cfbcf5e2c23ae
Filipe Lima <fhemrique55@gmail.com>
2024-08-29 03:44:13 -0300

    Finalização da Lógica de Lista de Telefones

--- complex-form/src/app/components/contact-informations/components/phone-list/phone-list.component.ts
+++ complex-form/src/app/components/contact-informations/components/phone-list/phone-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PhoneTypeEnum } from 'src/app/enums/phone-type.enum';
+import { IPhoneToDisplay } from 'src/app/interfaces/phone-to-display.interface';
 import { Iphone } from 'src/app/interfaces/user/phone.interface';
 import { PhoneList } from 'src/app/types/phone-list';
+import { phoneTypeDescriptionMap } from 'src/app/utils/phone-type-description-map';
 
 @Component({
   selector: 'app-phone-list',
@@ -9,25 +11,19 @@ import { PhoneList } from 'src/app/types/phone-list';
   styleUrls: ['./phone-list.component.scss']
 })
 export class PhoneListComponent implements OnChanges {
-  phoneListToDisplay: any[] = [];
+  phoneListToDisplay: IPhoneToDisplay[] = [];
   @Input({ required: true }) userPhoneList: PhoneList | undefined = []
 
   ngOnChanges(changes: SimpleChanges) {
     const PHONE_LIST_LOADED = Array.isArray(changes['userPhoneList'].currentValue);
-    
-    if (PHONE_LIST_LOADED){
+
+    if (PHONE_LIST_LOADED) {
       this.preparePhoneListToDisplay()
-    }   
+    }
   }
 
   preparePhoneListToDisplay() {
     this.phoneListToDisplay = [];
-
-    const phoneTypeDescriptionMap: {[key in PhoneTypeEnum]: string} = {
-      [PhoneTypeEnum.RESIDENTIAL]: 'Residencial',
-      [PhoneTypeEnum.MOBILE]: 'Celular',
-      [PhoneTypeEnum.EMERGENCY]: 'Emergêncial',
-    };
 
     Object.keys(phoneTypeDescriptionMap).map(Number).forEach((phoneType: number) => {
       const phoneFound = this.userPhoneList?.find((userPhone: Iphone) => userPhone.type === phoneType);
@@ -42,7 +38,7 @@ export class PhoneListComponent implements OnChanges {
   }
 
   formatPhoneNumber(phone: Iphone) {
-    
+
     return `${phone.internationalCode} ${phone.areaCode} ${phone.number}`;
   }
 
