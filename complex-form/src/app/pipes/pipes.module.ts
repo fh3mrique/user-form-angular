be487d23bf43e15c0f02028135b17ffcf0e0f543
Filipe Lima <fhemrique55@gmail.com>
2024-08-29 12:39:52 -0300

    Criação do Pipe de CPF

--- complex-form/src/app/pipes/pipes.module.ts
+++ complex-form/src/app/pipes/pipes.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { MaritalStatusPipe } from './marital-status.pipe';
+import { CpfPipe } from './cpf.pipe';
 
 @NgModule({
     declarations: [
-    MaritalStatusPipe
+    MaritalStatusPipe,
+    CpfPipe
   ],
     imports: [],
-    exports: [MaritalStatusPipe]
+    exports: [
+      MaritalStatusPipe,
+      CpfPipe
+    ]
 })
 export class PipesModule {
 
