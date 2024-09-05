ac0583f4f5e92d5bb3943c6b456fc39ba12c3cfc
Filipe Lima <fhemrique55@gmail.com>
2024-09-05 17:41:32 -0300

    Criação do Componente de Edição de Endereços

---
+++ complex-form/src/app/components/contact-informations-edit/components/address-list-edit/address-list-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-address-list-edit',
+  templateUrl: './address-list-edit.component.html',
+  styleUrls: ['./address-list-edit.component.scss']
+})
+export class AddressListEditComponent {
+
+}
