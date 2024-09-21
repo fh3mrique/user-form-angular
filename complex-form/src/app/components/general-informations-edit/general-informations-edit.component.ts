f35768d60a013ce70fca11d00abc5be447c494e1
Filipe Lima <fhemrique55@gmail.com>
2024-09-21 13:21:05 -0300

    Criação da Lógica de Autocomplete da Lista de Países

--- complex-form/src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ complex-form/src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { CountriesList } from 'src/app/types/countries-list';
 
 
@@ -8,17 +9,41 @@ import { CountriesList } from 'src/app/types/countries-list';
   templateUrl: './general-informations-edit.component.html',
   styleUrls: ['./general-informations-edit.component.scss']
 })
-export class GeneralInformationsEditComponent implements OnChanges {
+export class GeneralInformationsEditComponent implements OnInit, OnChanges {
 
   @Input({ required: true }) userForm!: FormGroup;
   @Input({ required: true }) countriesList: CountriesList = [];
 
+  countriesListFiltred: CountriesList = [];
+
+  ngOnInit() {
+    this.watchCountryFormChargesAndFilter()
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     console.log(changes);
+    this.countriesListFiltred = this.countriesList;
   }
 
   get emailControl(): FormControl {
     return this.userForm.get('generalInformations.email') as FormControl
   }
 
+  get countryControl(): FormControl {
+    return this.userForm.get('generalInformations.country') as FormControl
+  }
+
+  onCountrySelected(event: MatAutocompleteSelectedEvent) {
+    console.log(event.option.value);
+  }
+
+  private watchCountryFormChargesAndFilter() {
+    this.countryControl.valueChanges.subscribe(this.filterCountriesList.bind(this))
+  }
+
+  private filterCountriesList(searchTerm: string) {
+    this.countriesListFiltred = this.countriesList.filter(
+      (country) => country.name.toLowerCase().includes(searchTerm.toLowerCase().trim())
+    )
+  }
 }
