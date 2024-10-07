459664b06891bc1d1e69e7cf998546a63d7eb94e
Filipe Lima <fhemrique55@gmail.com>
2024-10-06 22:34:35 -0300

    Criação da Função de Conversão do UserForm para User: Parte 3

--- complex-form/src/app/utils/convert-user-form-to-user.ts
+++ complex-form/src/app/utils/convert-user-form-to-user.ts
@@ -1,18 +1,20 @@
-import { IuserForm, IUserFormGeneralInformations, IUserFormPhone } from "../interfaces/user-form.interface";
+import { IuserForm, IUserFormAddress, IUserFormGeneralInformations, IUserFormPhone } from "../interfaces/user-form.interface";
 import { IUser } from "../interfaces/user/user.interface";
+import { AddressList } from "../types/address-list";
 import { PhoneList } from "../types/phone-list";
 import { convertDateObjToPtBrDate } from "./convert-date-obj-to-pt-br-date";
 
 export const convertUserFormToUser = (userForm: IuserForm): IUser => {
     let newUser: Partial<IUser> = {} as IUser;
 
-    newUser = {... convertGeneralInformations(userForm.generalInformations)};
-    newUser.phoneList = {... convertPhoneList(userForm.contactInformations.phoneList)};
+    newUser = { ...convertGeneralInformations(userForm.generalInformations) };
+    newUser.phoneList = { ...convertPhoneList(userForm.contactInformations.phoneList) };
+    newUser.addressList = { ...convertAdressList(userForm.contactInformations.addressList) };
 
     return newUser as IUser;
 };
 
-const convertGeneralInformations = (generalInformations: IUserFormGeneralInformations): Partial<IUser> =>{
+const convertGeneralInformations = (generalInformations: IUserFormGeneralInformations): Partial<IUser> => {
     return {
         name: generalInformations.name,
         email: generalInformations.email,
@@ -27,10 +29,23 @@ const convertGeneralInformations = (generalInformations: IUserFormGeneralInforma
 const convertPhoneList = (phoneList: IUserFormPhone[]): PhoneList => {
     const newUserPhoneList: PhoneList = phoneList.map((phone) => ({
         type: phone.type,
-        internationalCode: phone.Number.substring(0,2),
-        areaCode: phone.Number.substring(2,4),
+        internationalCode: phone.Number.substring(0, 2),
+        areaCode: phone.Number.substring(2, 4),
         number: phone.Number.substring(4),
     }));
 
     return newUserPhoneList;
+};
+
+const convertAdressList = (addressList: IUserFormAddress[]): AddressList => {
+    const newUserAdressList: AddressList = addressList.map((address) => ({
+        type: address.type,
+        street: address.street,
+        complement: address.complement,
+        city: address.city,
+        state: address.state,
+        country: address.country
+    }));
+
+    return newUserAdressList;
 };
